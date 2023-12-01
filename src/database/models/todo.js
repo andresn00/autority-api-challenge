2ea268fc2f41d394271d1242f048662a8a0cc49b
andresn00 <andreslozada00@hotmail.com>
2023-12-01 16:51:41 -0500

    Add Validations to Todo model

--- src/database/models/todo.js
+++ src/database/models/todo.js
@@ -8,20 +8,36 @@ export default function (sequelize) {
     name: {
       allowNull: false,
       type: DataTypes.STRING(80),
+      validate: {
+        is: /^[a-zA-Z0-9 -]*$/
+      },
     },
     description: {
       allowNull: false,
       type: DataTypes.TEXT,
+      validate: {
+        is: /^[a-zA-Z0-9 -]*$/
+      },
     },
     author: {
       allowNull: false,
       type: DataTypes.STRING(50),
+      validate: {
+        is: /^[a-zA-Z0-9 -]*$/
+      },
     },
     isComplete: {
       allowNull: false,
       type: DataTypes.BOOLEAN,
+      validate: {
+        isBoolean: (value) => {
+          if (typeof value !== 'boolean') {
+            throw new Error("Not boolean")
+          }
+          return true
+        }
+      },
     },
-
   }, {
     modelName: 'todo',
     sequelize,
